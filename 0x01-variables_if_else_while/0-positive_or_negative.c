8af346e79d1759bf2c407f12e5b97368a5a755c1
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-04 04:09:09 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main ->  assign a random number to the variable n each time it is executed 
  * and  print the last digit of the number stored in the variable n
@@ -11,7 +12,6 @@ int main(void)
 {
 int n;
 
-  
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
