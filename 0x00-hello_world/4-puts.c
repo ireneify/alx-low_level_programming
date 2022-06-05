d06f06ea2db199f53c5f49490b023a2cf3c45c97
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-05 01:41:15 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry point
  * Return: Always
- 
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
