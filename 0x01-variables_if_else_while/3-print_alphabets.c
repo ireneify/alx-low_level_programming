6b1a697c3a85d9a1e025236eb61c07d4a059097a
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-05 12:32:55 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	int ch;
-	for(ch = 97; ch <= 122; ch++)
-	{
-		putchar(ch);
-	}
-	for(ch = 65; ch >= 90; ch++)
-	{
-		putchar(ch);
-	}
-	putchar(10); /*this is ascii code for newline*/
-	return (0);
+int ch;
+for(ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for(ch = 65; ch >= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10); /*this is ascii code for newline*/
+return (0);
 }
