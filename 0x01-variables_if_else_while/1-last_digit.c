eafa3d3bca9cc83ea0ae39d4bac2d678f74ef1ef
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-05 12:29:46 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,25 +15,21 @@ int x;
 srand(time(0));
 
 n = rand() - RAND_MAX / 2;
-  x = n % 10;  
+x = n % 10;  
 
-    printf("last digit %d is %d", n, x);
+printf("last digit %d is %d", n, x);
 if ( x > 5 )
 {  
-    printf("greater than 5");
+printf("greater than 5");
 }
 if ( x ==0 )
 {    
-    printf("and is 0");
+printf("and is 0");
 }
 if ( x < 6 && x !=0 )
 {   
-    printf("and is less than 6 not 0");
+printf("and is less than 6 not 0");
 }
-    printf("\n");
-
+printf("\n");
 return (0);
-
 }
-
-
