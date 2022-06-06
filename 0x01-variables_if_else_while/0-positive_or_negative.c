9828bf06e07901b597b9e267cfcac3414cf310f9
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-06 03:29:51 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main-> assign a random number to the variable n each time it is executed 
+ * maon -> assign a randon number to a variable n each time it is exercuted 
  * and  print the last digit of the number stored in the variable n
- *
  * Return: Always (0) sucess
  */
 int main(void)
