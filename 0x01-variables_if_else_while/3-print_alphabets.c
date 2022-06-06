0814e72b4c8d5b47c840591cacdf32a57cf05dc8
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-06 04:12:53 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,8 @@
 /**
  * main -> assign a random number to the variable n each time it is exercuted
  * and print the last digit of the number stored in the variable n
- * Return: Always 0 (Sucess)
- */
-int main(void)
+ * Return: Always 0 
+int mode(void)
 {
 int ch;
 for (ch = 97; ch <= 122; ch++)
