699d5f5aa6c80eedaca6b2c769e0e2587c24c2df
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-07 18:01:18 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,16 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int a;
 
 	if (n < 0)
 		n = -n;
 
-	x = n % 10;
+	a = n % 10;
 
 	if (x < 0)
-		x = -x;
+		a = -a;
 
-	_putchar(x + '0');
+	_putchar(a + '0');
 
-	return (x);
+	return (a);
