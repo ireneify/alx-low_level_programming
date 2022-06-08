061306dfdd1f122815e5c3fd8e63285882d0b17c
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-07 17:30:06 -0700

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,9 @@
 #include "main.h"
-
-
-
 /**
- *
- * * _abs -> return absolute value of a number
- *
- * * @n: parameter
- *
- * * Return: absolute value of a number
- *
- * */
+ * _abs -> return absolute value of a number
+ * @n: parameter
+ * Return: absolute value of a number
+ * /
 
 int _abs(int n)
 
