cb98dd4964a1becc02d5567b7939cd37a6a00a4b
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-07 18:40:43 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,30 @@
-
 #include "main.h"
-
 /**
 *times_table -> time table
 */
 void times_table(void)
 {
-	int x, y, k;
+	int a, b, c;
 
-	for (x = 0; x < 10; x++)
+	for (a = 0; a < 10; a++)
 	{
-		for (y = 0; y < 10; y++)
+		for (b = 0; b < 10; b++)
 		{
-			k = x * y;
-			if (y == 0)
+			c = a * b;
+			if (b == 0)
 				_putchar(k + '0');
-			if (y != 0 && k < 10)
+			if (b != 0 && c < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
+				_putchar(c + '0');
+			} else if (c >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
