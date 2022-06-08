167d2417a5462ec121ddba27b6b542da96c154af
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-07 19:23:10 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,30 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* print_to_98 - prints to 98
-* @n: the starting point
+* add -> adds two number
+* @x : param1
+* @y : param2
+* Return: x + y
 */
-void print_to_98(int n)
+
+int add(int x, int y)
 {
-	if (n <= 98)
-	{
-		while (n <= 98)
-		{
-			if (n == 98)
-				printf("%d\n", n);
-			else
-				printf("%d, ", n);
-			n = n + 1;
-		}
-	} else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			if (n == 98)
-				printf("%d\n", n);
-			else
-				printf("%d, ", n);
-			n = n - 1;
-		}
-	}
+	return (x + y);
 }
