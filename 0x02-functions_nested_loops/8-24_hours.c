ac8061344332226a8660df8632ac77fa445d6df2
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-07 18:26:32 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,78 +1,39 @@
 #include "main.h"
-
 /**
- *
- * jack_bauer -> prints 24 hours
- *
- */
-
+* jack_bauer -> prints 24 hours
+*/
 void jack_bauer(void)
-
 {
+	int k, j;
 
-		int i, j;
+	for (k = 0; k < 24; k++)
+	{
+		for (l = 0; l < 60; l++)
+		{
+			if (k < 10)
+			{
+				_putchar('0');
+				_putchar(k + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (l < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(l + '0');
+			}
+			else if (l >= 10)
+			{
+				_putchar(':');
+				_putchar((l / 10) + '0');
+				_putchar((l % 10) + '0');
+			}
+			_putchar('\n');
+		}
 
-
-
-			for (i = 0; i < 24; i++)
-
-					{
-
-			for (j = 0; j < 60; j++)
-
-			
-		            {
-
-															if (i < 10)
-
-																			{
-
-																								_putchar('0');
-
-																												_putchar(i + '0');
-
-																															}
-
-																		else if (i >= 10)
-
-																						{
-
-																											_putchar((i / 10) + '0');
-
-																															_putchar((i % 10) + '0');
-
-																																		}
-
-																					if (j < 10)
-
-																									{
-
-																														_putchar(':');
-
-																																		_putchar('0');
-
-																																						_putchar(j + '0');
-
-																																									}
-
-																								else if (j >= 10)
-
-																												{
-
-																																	_putchar(':');
-
-																																					_putchar((j / 10) + '0');
-
-																																									_putchar((j % 10) + '0');
-
-																																												}
-
-																											_putchar('\n');
-
-																													}
-
-
-
-									}
-
+	}
 }
