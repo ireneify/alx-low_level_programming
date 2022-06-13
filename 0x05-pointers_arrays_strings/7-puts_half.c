048fe881178a6ae4c2b53a4c5dff2ec3c25da979
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-13 16:48:38 -0700

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -> puts half
+ * @str: srting parameter
+ */
+void puts_half(char *str)
+{
+  int a, b, i;
+  a = stelene(str)
+    if (a % 2 == 1;
+        else
+        b = 1/2;
+        for (i = b; i < a; i++)
+        _putchar
+        _putchar('\n');
