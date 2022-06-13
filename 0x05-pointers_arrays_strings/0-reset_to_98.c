9f01087362e60636d5aa81864b96b97577785363
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-13 13:31:52 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,8 @@
 /**
 * reset_to_98 -> a function to update the reference of a pointer
 * @n: pointer to n to be updated
-* Return: Always 0
 */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (0);
 }
