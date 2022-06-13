96b1980b5699037416f65a0bd5e480b221ec7560
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-13 15:29:12 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,8 +17,7 @@ void rev_strings(char *s)
       {
           tmp = s[i];
           s[i] = s[length2];
-          s[length2] =tmp;
+          s[length2] = tmp;
           length2 -= 1;
       }
-}
-     
+}     
