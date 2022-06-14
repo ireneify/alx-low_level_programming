3b731acd43cf6644eeeb59bc0f1d248ab8f11d46
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-13 17:06:00 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,14 @@
  */
 void puts_half(char *str)
 {
-  int a, b, i;
-  a = strlen(str);
-    if (a % 2 == 1)
-      b = a / 2 + 1;
-        else
-        b = a / 2;
-        for (i = b; i < a; i++)
-        _putchar(str[i]);
-        _putchar('\n');
+int a, b, i;
+  
+a = strlen(str);
+if (a % 2 == 1)
+b = a / 2 + 1;
+else
+b = a / 2;
+for (i = b; i < a; i++)
+_putchar(str[i]);
+_putchar('\n');
 }
