cddca3a9e32fbb9ae3a2595577e8b71228ea3316
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-16 15:42:46 -0700

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,16 +6,16 @@
 */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
-        
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
-	}
+int *p, i, aux, k;
+
+p = a;
+for (i = 0; i < n; i++)
+p++;
+for (k = 0; k < i / 2; k++)
+{
+aux = a[k];
+a[k] = *p;
+*p = aux;
+p--;
+}
 }
