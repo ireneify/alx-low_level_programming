ff43b10d77de72aa3681485df47b1a870399b917
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-20 12:51:26 -0700

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0, b = 0;
+
+	while (haystack[y])
+	{
+		while (needle[y])
+		{
+			if (haystack[x + y] != needle[y])
+			{
+				break;
+			}
+
+			y++;
+		}
+
+		if (needle[y] == '\0')
+		{
+			return (haystack + x);
+		}
+
+		x++;
+	}
+
+	return ('\0');
+}
