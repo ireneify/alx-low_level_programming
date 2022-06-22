486beb839e49f8dce099f754254dcc21f8fa5ef3
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-06-22 14:08:13 -0700

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
 * _strlen_recursion -> function to get length of string
 * @s: string param
 * Return: length of string
 */
-int _strlen_recursion(char *s) 
-
+int _strlen_recursion(char *s)
 {	
 	if (*s == '\0')
 		return (0);
