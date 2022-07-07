2f7ff8c505ac246f2cc1d1408e79b23ec0e0794f
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-07 12:52:34 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,6 +10,9 @@ int sum_them_all(const unsigned int n, ...);
 	va_list args;
 	unsigned int i;
 	int sum = 0;
+	
+	if (n == 0)
+		return (0);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
@@ -19,4 +22,3 @@ int sum_them_all(const unsigned int n, ...);
 	va_end(args);
 	return (sum);
 }
-
