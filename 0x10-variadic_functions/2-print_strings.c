66c33f77a89d0e296b700d91f5f419453c120dc7
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-07 14:33:48 -0700

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * print_string - print string followed by a new line
  * @seperators: string to be printed between strings
