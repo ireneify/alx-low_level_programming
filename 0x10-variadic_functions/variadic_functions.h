dffb8bae37f22fe402db03eb493f2186081685c2
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-07 13:46:00 -0700

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef _VAR_FUNCS_
 #define _VAR_FUNCS_
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
