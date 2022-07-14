8f5fb8fca1e906085fc2e7da24a1c544d257c935
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-14 14:14:27 -0700

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * free_list - frees a list
  * @head: head of the linked list.
  * Return: no return.
  */
-void free_list(list_t *head);
+void free_list(list_t *head)
 
 {
 	list_t *current;
