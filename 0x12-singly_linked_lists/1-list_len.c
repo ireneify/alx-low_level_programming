947b2f3dddd77caec048044ba3d0a2c8089b73fe
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-14 13:42:32 -0700

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
  * list_len - returns then number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
 	size_t nelem;
 
@@ -15,7 +13,7 @@ size_t list_len(const list_t *h);
 	while (h != NULL)
 	{
 		h = h->next;
-		nelem++
+		nelem++;
 	}
 	return (nelem);
 }
