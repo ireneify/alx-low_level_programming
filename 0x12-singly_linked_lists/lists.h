0e14ce8b38fbc9c41a3e8e2cd31bf5768b91ee79
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-25 14:43:19 -0700

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,29 +1,23 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Return: void
  */
-typedef struct list_s
+void free_list(list_t *head)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	list_t *current, *next;
 
-int _putchar(char);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
 
-#endif
+}
