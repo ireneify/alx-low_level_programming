3679b048b6827152e7b0cfd1fee834e40e735856
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-07-29 14:12:09 -0700

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -198,20 +198,20 @@ void elf_type(const unsigned char *buffer, int big_endian)
 		"CORE (Core file)"
 	};
 	unsigned int type;
+	
+	if (big_endian)
+		type = 0x100 * buffer[16] + buffer[17];
+	else
+		type = 0x100 * buffer[17] + buffer[16];
 
-	printf("  %-34s ", "Type:");
-    if (big_endian)
-    type = 0x100 * buffer[16] + buffer[17];
-    else
-    type = 0x100 * buffer[17] + buffer[16];
-    if (type < 5)
-    printf("%s\n", type_table[type]);
-    else if (type >= ET_LOOS && type <= ET_HIOS)
-    printf("OS Specific: (%4x)\n", type);
-    else if (type >= ET_LOPROC && type <= ET_HIPROC)
-    printf("Processor Specific: (%4x)\n", type);
-    else
-    printf("<unknown: %x>\n", type);
+	if (type < 5)
+		printf("%s\n", type_table[type]);
+	else if (type >= ET_LOOS && type <= ET_HIOS)
+		printf("OS Specific: (%4x)\n", type);
+	else if (type >= ET_LOPROC && type <= ET_HIPROC)
+		printf("Processor Specific: (%4x)\n", type);
+	else
+		printf("<unknown: %x>\n", type);
     }
     
 /**
